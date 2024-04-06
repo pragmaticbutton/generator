b3ab7487cd5445cac1cddc969825ac93cc4f62f3
pragmaticbutton <jelenaziv96@gmail.com>
2024-04-06 15:56:09 +0200

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"generator/internal/project"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
-	"text/template"
 
 	cli "github.com/urfave/cli/v2"
 )
@@ -15,22 +12,21 @@ import (
 func main() {
 
 	app := &cli.App{
-		// TODO: improve these parameters
-		Name:  "Generate new project",
-		Usage: "go run cmd/main.go newProjectName",
-		Args:  true,
-		//ArgsUsage:            "Specify new project's name",
+		Name:                 "Generator",
+		Usage:                "Generates new project out of existing templates",
+		UsageText:            "go run ./... [location] [name]",
+		Args:                 true,
 		EnableBashCompletion: true,
 		Action: func(ctx *cli.Context) error {
 			if ctx.NArg() != 2 {
-				os.Exit(1)
+				return fmt.Errorf("expected number of parameters is 2")
 			}
 
-			loc := ctx.Args().Get(0)
+			location := ctx.Args().Get(0)
 			name := ctx.Args().Get(1)
-			if err := generateProject(loc, name); err != nil {
-				fmt.Printf("%v\n", err)
-				os.Exit(1)
+			p := project.New(location, name)
+			if err := p.Generate(); err != nil {
+				return fmt.Errorf("%v", err)
 			}
 
 			return nil
@@ -41,63 +37,4 @@ func main() {
 		log.Fatal(err)
 	}
 
-}
-
-func generateProject(location, name string) error {
-
-	templateDir := "templates/go/simple"
-
-	if err := copyContentsOfDir(templateDir, location, name); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func copyContentsOfDir(dirPath, location, name string) error {
-
-	p := project.New(name)
-
-	err := filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
-
-		if err != nil {
-			return err
-		}
-
-		relPath, relErr := filepath.Rel(dirPath, path)
-		if relErr != nil {
-			return relErr
-		}
-
-		destPath := filepath.Join(location, name, relPath)
-
-		if d.IsDir() {
-			if err := os.Mkdir(destPath, 0744); err != nil {
-				return err
-			}
-		} else {
-
-			dst, err := os.OpenFile(destPath, os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				return err
-			}
-
-			tmpl, err := template.ParseFiles(path)
-			if err != nil {
-				return err
-			}
-
-			err = tmpl.Execute(dst, p)
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
--- internal/project/project.go
+++ internal/project/project.go
@@ -1,11 +1,100 @@
 package project
 
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+const (
+	templateDir = "templates/go/simple"
+)
+
 type Project struct {
-	Name string
+	Location string
+	Name     string
 }
 
-func New(name string) *Project {
+func New(location string, name string) *Project {
 	return &Project{
-		Name: name,
+		Location: location,
+		Name:     name,
 	}
+}
+
+func (p *Project) Generate() error {
+
+	if err := p.validate(); err != nil {
+		return err
+	}
+
+	err := filepath.WalkDir(templateDir, func(path string, d fs.DirEntry, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		relPath, relErr := filepath.Rel(templateDir, path)
+		if relErr != nil {
+			return relErr
+		}
+
+		destPath := filepath.Join(p.Location, p.Name, relPath)
+
+		if d.IsDir() {
+			if err := os.Mkdir(destPath, 0744); err != nil {
+				return err
+			}
+		} else {
+
+			dst, err := os.OpenFile(destPath, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				return err
+			}
+
+			tmpl, err := template.ParseFiles(path)
+			if err != nil {
+				return err
+			}
+
+			err = tmpl.Execute(dst, p)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Project) validate() error {
+
+	d, err := os.Stat(p.Location)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("%v doesn't exist", p.Location)
+		}
+		return err
+	}
+
+	if !d.IsDir() {
+		return fmt.Errorf("%s is not a valid directory path", p.Location)
+	}
+
+	_, err = os.Open(filepath.Join(p.Location, p.Name))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+
+	return fmt.Errorf("project %s already exists in %s", p.Name, p.Location)
 }
